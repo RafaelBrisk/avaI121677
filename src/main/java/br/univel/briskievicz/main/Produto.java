404d927b072c6085aa8b587218c29d602a5735d9
RBRISKIEVICZ <rafael_27-05@hotmail.com>
2017-06-12 21:16:25 -0300

    Criação do Projeto.

---
+++ src/main/java/br/univel/briskievicz/main/Produto.java
@@ -0,0 +1,48 @@
+package br.univel.briskievicz.main;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Produto {
+	
+	private Long id;
+	
+	private String nome;
+	
+	private BigDecimal valor;
+	
+	private LocalDateTime dataVencimento;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public LocalDateTime getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(LocalDateTime dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+}
